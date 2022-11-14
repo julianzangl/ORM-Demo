f0a6ed5e987989923ab35d0e70e76bc7371b2913
JuleZ_2k4 <8096@htl.rennweg.at>
2022-11-14 10:20:42 +0100

    core finished

---
+++ src/Form/QuoteType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Quote;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('quote')
+            ->add('character_name')
+            ->add('movie')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Quote::class,
+        ]);
+    }
+}
